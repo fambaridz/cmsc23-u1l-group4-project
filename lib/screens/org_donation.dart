7f6be590fe02298ecee51c44b5ffff9c4e2717d3
AldCristobal <alcristobal1@up.edu.ph>
2024-05-13 05:29:23 +0800

    chore: implemented donation model to sample list

--- lib/screens/org_donation.dart
+++ lib/screens/org_donation.dart
@@ -1,3 +1,4 @@
+import 'package:cmsc23_project/model/donation.dart';
 import 'package:flutter/material.dart';
 
 class OrganizationDonationPage extends StatefulWidget {
@@ -8,17 +9,40 @@ class OrganizationDonationPage extends StatefulWidget {
 }
 
 class _OrganizationDonationPageState extends State<OrganizationDonationPage> {
-  final List<String> donationList = [
-    "Donation 1",
-    "Donation 2",
-    "Donation 3",
-    "Donation 4",
-    "Donation 5",
-    "Donation 6",
-    "Donation 7",
-    "Donation 8",
-    "Donation 9",
-    "Donation 10",
+  final List<Donation> donationList = [
+    Donation(
+      id: "1",
+      donor: "Juan Dela Cruz",
+      category: "Clothes",
+      weight: 5,
+      address: "1234 Donations St.",
+      contactNo: "09123456789",
+      pickUpDateTime: "2022-12-31 23:59:59",
+      dropOffDateTime: "2023-01-01 00:00:00",
+      photo: "photo.jpg"
+    ),
+    Donation(
+      id: "2",
+      donor: "Pedro Penduko",
+      category: "Books",
+      weight: 10,
+      address: "5678 Donations St.",
+      contactNo: "09876543210",
+      pickUpDateTime: "2022-12-31 23:59:59",
+      dropOffDateTime: "2023-01-01 00:00:00",
+      photo: "photo.jpg"
+    ),
+    Donation(
+      id: "3",
+      donor: "Maria Makiling",
+      category: "Food",
+      weight: 15,
+      address: "91011 Donations St.",
+      contactNo: "09123456789",
+      pickUpDateTime: "2022-12-31 23:59:59",
+      dropOffDateTime: "2023-01-01 00:00:00",
+      photo: "photo.jpg"
+    )
   ];
   
   @override
@@ -36,9 +60,13 @@ class _OrganizationDonationPageState extends State<OrganizationDonationPage> {
             itemBuilder: (context, index) {
               return ListTile(
                 leading: Text("Status"),
-                title: Text(donationList[index]),
+                title: Text(donationList[index].id),
                 trailing: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/org-home/donation/details", arguments: {
+                      "details": donationList[index]
+                    });
+                  },
                   child: Text("View"),
                 ),
               );
