0b48404fd1fb06c85076f51272c8c623cf164d38
Famela Barida <fdbarida@up.edu.ph>
2024-05-13 16:35:24 +0800

    feat: added initial list of organizations for UI

--- lib/screens/donor_home.dart
+++ lib/screens/donor_home.dart
@@ -9,31 +9,60 @@ class DonorHomePage extends StatefulWidget {
 }
 
 class _DonorHomePageState extends State<DonorHomePage> {
+  final List<String> organizations = [
+    'Organization A',
+    'Organization B',
+    'Organization C',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const DonorDrawer(),
+      drawer: DonorDrawer(),
       appBar: AppBar(
         title: Text("Donor Home"),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              SizedBox(height: 100),
-              Text(
-                "Sign In",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Center(
+        child: organizations.isEmpty
+            ? const Text("No organizations available")
+            : ListView(
+                padding: const EdgeInsets.all(10),
+                children: organizations.map((organization) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[100],
+                  ),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    title: Text(organization,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: ElevatedButton(
+                      onPressed: () {
+                        // Navigator.pushNamed(context, "/org-home");
+                      },
+                      child: Text("Donate",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightBlue[200],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ))
+                .toList(),
               ),
-              
-            ],
-          ),
-        ),
-      )
+      ),
     );
   }
 }
