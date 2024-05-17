a5aea70597f532b0c130d347ca5f764829d28865
AldCristobal <alcristobal1@up.edu.ph>
2024-05-18 00:40:07 +0800

    chore: add route for donation drive details

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cmsc23_project/model/donation.dart';
+import 'package:cmsc23_project/model/donation_drive.dart';
 import 'package:cmsc23_project/model/organization.dart';
 import 'package:cmsc23_project/screens/admin_home.dart';
 import 'package:cmsc23_project/screens/donor_donation.dart';
@@ -6,9 +7,9 @@ import 'package:cmsc23_project/screens/donor_home.dart';
 import 'package:cmsc23_project/screens/donor_org_details.dart';
 import 'package:cmsc23_project/screens/donor_profile.dart';
 import 'package:cmsc23_project/screens/landing.dart';
+import 'package:cmsc23_project/screens/org_donation_drive_details.dart';
 import 'package:cmsc23_project/screens/sign_in.dart';
 import 'package:cmsc23_project/screens/sign_up.dart';
-import 'package:cmsc23_project/screens/org_donation.dart';
 import 'package:cmsc23_project/screens/org_donation_details.dart';
 import 'package:cmsc23_project/screens/org_donation_drive.dart';
 import 'package:cmsc23_project/screens/org_profile.dart';
@@ -44,12 +45,14 @@ void main() {
         "/donor-donation": (context) => const DonorDonationPage(),
         // organization routes
         "/org-home": (context) => const OrganizationHomePage(),
-        "/org-home/donation": (context) => const OrganizationDonationPage(),
         "/org-home/donation/details": (context) => OrganizationDonationDetails(
           ModalRoute.of(context)!.settings.arguments as Map<String, Donation>),
         "/org-home/profile": (context) => OrganizationDetails(
           ModalRoute.of(context)!.settings.arguments as Map<String, Organization>),
         "/org-home/donation-drive": (context) => const OrganizationDonationDrivePage(),
+        "/org-home/donation-drive/details": (context) => OrganizationDonationDriveDetails(
+          ModalRoute.of(context)!.settings.arguments as Map<String, DonationDrive>),
+        
       },
     ),
   );
