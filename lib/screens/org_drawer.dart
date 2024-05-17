261c5f4f8bd6ded8a3eb9e6b2e912ac9ed318057
AldCristobal <alcristobal1@up.edu.ph>
2024-05-17 21:12:50 +0800

    feat: add org drawer

---
+++ lib/screens/org_drawer.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class OrganizationDrawer extends StatefulWidget {
+  const OrganizationDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<OrganizationDrawer> createState() => _OrganizationDrawerState();
+}
+
+class _OrganizationDrawerState extends State<OrganizationDrawer> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          Container(
+            height: 100,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.lightBlue[200],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "John Doe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/donor-profile");
+                        }, 
+                        child: Text(
+                          "View Profile",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlue[400],
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+            onTap: () {
+              Navigator.pushNamed(context, "/donor-home");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () {
+              Navigator.pushNamed(context, "/");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
