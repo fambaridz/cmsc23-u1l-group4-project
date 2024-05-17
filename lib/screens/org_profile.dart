754fd902905bc26a4e7696250005962c6d36e466
AldCristobal <alcristobal1@up.edu.ph>
2024-05-18 01:06:35 +0800

    fix: remove flexible widgets

--- lib/screens/org_profile.dart
+++ lib/screens/org_profile.dart
@@ -103,131 +103,125 @@ class OrganizationDetails extends StatelessWidget {
             Expanded(
               child: Container(
                 width: double.infinity,
-                child: Flexible(
-                  fit: FlexFit.tight,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30.0),
-                        topRight: Radius.circular(30.0),
-                      ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
                     ),
-                    child: Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "About Us",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "About Us",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(height: 15),
-                          Expanded(
+                        ),
+                        SizedBox(height: 15),
+                        Expanded(
+                          child: Container(
+                            width: double.infinity,
                             child: Container(
-                              width: double.infinity,
-                              child: Flexible(
-                                fit: FlexFit.tight,
-                                child: Container(
-                                    child: Column(
-                                  children: [
-                                    Text(
-                                      details.aboutUs,
-                                      textAlign: TextAlign.justify,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    details.aboutUs,
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                      fontSize: 20,
                                     ),
-                                    SizedBox(height: 20),
-                                    Divider(),
-                                    SizedBox(height: 20),
-                                    Text(
-                                      "Donation Drives",
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Divider(),
+                                  SizedBox(height: 20),
+                                  Text(
+                                    "Donation Drives",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
                                     ),
-                                    SizedBox(height: 20),
-                                    Expanded(
-                                      child: donationDriveList.isEmpty
-                                          ? const Text(
-                                              "No donation drives available")
-                                          : ListView(
-                                              padding: const EdgeInsets.all(10),
-                                              children: donationDriveList
-                                                  .map((donationDrive) =>
-                                                      GestureDetector(
-                                                        onTap: () {
-                                                          Navigator.pushNamed(
-                                                              context,
-                                                              "/org-home/donation-drive/details",
-                                                              arguments: {
-                                                                "details":
-                                                                    donationDrive
-                                                              });
-                                                        },
-                                                        child: Container(
-                                                          decoration:
-                                                              BoxDecoration(
-                                                            borderRadius:
-                                                                BorderRadius
-                                                                    .circular(
-                                                                        10),
-                                                            color: Colors
-                                                                .grey[100],
-                                                          ),
-                                                          margin:
-                                                              EdgeInsets.all(3),
-                                                          child: ListTile(
-                                                            contentPadding:
-                                                                EdgeInsets.all(
-                                                                    20),
-                                                            leading:
-                                                                FractionallySizedBox(
-                                                              heightFactor: 1,
-                                                              widthFactor: 0.25,
-                                                              child: Icon(
-                                                                donationDrive
-                                                                        .status
-                                                                    ? Icons
-                                                                        .check_circle
-                                                                    : Icons
-                                                                        .cancel,
-                                                                color: donationDrive
-                                                                        .status
-                                                                    ? Colors
-                                                                        .green
-                                                                    : Colors
-                                                                        .red,
-                                                                size: 50,
-                                                              ),
-                                                            ),
-                                                            title: Text(
+                                  ),
+                                  SizedBox(height: 20),
+                                  Expanded(
+                                    child: donationDriveList.isEmpty
+                                        ? const Text(
+                                            "No donation drives available")
+                                        : ListView(
+                                            padding: const EdgeInsets.all(10),
+                                            children: donationDriveList
+                                                .map((donationDrive) =>
+                                                    GestureDetector(
+                                                      onTap: () {
+                                                        Navigator.pushNamed(
+                                                            context,
+                                                            "/org-home/donation-drive/details",
+                                                            arguments: {
+                                                              "details":
+                                                                  donationDrive
+                                                            });
+                                                      },
+                                                      child: Container(
+                                                        decoration:
+                                                            BoxDecoration(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(10),
+                                                          color:
+                                                              Colors.grey[100],
+                                                        ),
+                                                        margin:
+                                                            EdgeInsets.all(3),
+                                                        child: ListTile(
+                                                          contentPadding:
+                                                              EdgeInsets.all(
+                                                                  20),
+                                                          leading:
+                                                              FractionallySizedBox(
+                                                            heightFactor: 1,
+                                                            widthFactor: 0.25,
+                                                            child: Icon(
                                                               donationDrive
-                                                                  .name,
-                                                              style: TextStyle(
-                                                                fontSize: 17,
-                                                                fontWeight:
-                                                                    FontWeight
-                                                                        .bold,
-                                                              ),
+                                                                      .status
+                                                                  ? Icons
+                                                                      .check_circle
+                                                                  : Icons
+                                                                      .cancel,
+                                                              color:
+                                                                  donationDrive
+                                                                          .status
+                                                                      ? Colors
+                                                                          .green
+                                                                      : Colors
+                                                                          .red,
+                                                              size: 50,
+                                                            ),
+                                                          ),
+                                                          title: Text(
+                                                            donationDrive.name,
+                                                            style: TextStyle(
+                                                              fontSize: 17,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold,
                                                             ),
                                                           ),
                                                         ),
-                                                      ))
-                                                  .toList(),
-                                            ),
-                                    ),
-                                  ],
-                                )),
+                                                      ),
+                                                    ))
+                                                .toList(),
+                                          ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
