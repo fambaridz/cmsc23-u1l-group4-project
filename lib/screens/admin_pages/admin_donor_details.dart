66957f331126b05b532c201fb0ee06fba2a0a72f
jk-borja <jbborja@up.edu.ph>
2024-05-18 15:06:20 +0800

    chore: edited details page for donor in admin

--- lib/screens/admin_pages/admin_donor_details.dart
+++ lib/screens/admin_pages/admin_donor_details.dart
@@ -13,16 +13,56 @@ class AdminDonorDetailsPage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-                height: 200,
-                width: 200,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/profile.png')))),
-            Text("Jane Smith"),
-            Text("@therealJS"),
-            Text("Address/es: "),
-            Text("Contact Number: ")
+                width: double.infinity,
+                height: 400,
+                color: Colors.lightBlue[200],
+                child: Center(
+                    child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                        height: 160,
+                        width: 160,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/profile.png')))),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Jane Smith",
+                      style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      "@therealJS",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    const SizedBox(
+                      width: 350,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 15),
+                          Text("Contact Number: ",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white)),
+                          Text("Address/es: ",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white)),
+                        ],
+                      ),
+                    )
+                  ],
+                ))),
+            const SizedBox(height: 25),
+            const Text("Donations by Donor A",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline)),
           ],
         ),
       ),
