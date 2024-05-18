2a5d827e399d310b8be080d53117af90d7783880
jk-borja <jbborja@up.edu.ph>
2024-05-18 16:27:29 +0800

    chore: edited details page for approvals in admin

--- lib/screens/admin_pages/admin_approval_details.dart
+++ lib/screens/admin_pages/admin_approval_details.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../model/organization.dart';
+
 class AdminApprovalDetailsPage extends StatelessWidget {
   const AdminApprovalDetailsPage({super.key});
   @override
   Widget build(BuildContext context) {
+    var organization = Organization(
+        id: '1',
+        name: 'Organization A',
+        aboutUs: 'We are an organization.',
+        status: false
+        // proof of legitimacy
+        );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Organization for Approval"),
@@ -11,17 +21,54 @@ class AdminApprovalDetailsPage extends StatelessWidget {
       body: Center(
           child: Column(
         children: [
-          SizedBox(
-            height: 40,
-          ),
-          Text("Organization details"),
+          Center(
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.lightBlue[200],
+                child: Center(
+                  child: Text(
+                    "${organization.name}",
+                    style: TextStyle(fontSize: 35, color: Colors.white),
+                  ),
+                )),
+            SizedBox(height: 50),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("About Us:",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Text("${organization.aboutUs}", style: TextStyle(fontSize: 20)),
+                SizedBox(height: 30),
+                Text("Status:",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                organization.status
+                    ? Text(
+                        "Open",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    : Text(
+                        "Closed",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                SizedBox(height: 30),
+                Text("Proof of Legitimacy:",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              ],
+            )
+          ])),
           SizedBox(height: 40),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlue[200]),
+                backgroundColor: Colors.lightBlue[400]),
             child: Text(
               "Approve",
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(fontSize: 18, color: Colors.white),
             ),
             onPressed: () {},
           )
