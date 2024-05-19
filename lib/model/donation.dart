c646e94eca953a16a4b8a8e2b5093e89b19c70ba
jk-borja <jbborja@up.edu.ph>
2024-05-19 18:47:01 +0800

    chore: added json conversion in donation model

--- lib/model/donation.dart
+++ lib/model/donation.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Donation {
   String id;
   String donor;
@@ -21,4 +23,38 @@ class Donation {
       required this.dropOffDateTime,
       this.photo,
       required this.status});
+
+  factory Donation.fromJson(Map<String, dynamic> json) {
+    return Donation(
+        id: json['id'],
+        donor: json['donor'],
+        category: json['category'],
+        weight: json['weight'],
+        address: json['address'],
+        contactNo: json['contactNo'],
+        pickUpDateTime: json['pickUpDateTime'],
+        dropOffDateTime: json['dropOffDateTime'],
+        photo: json['photo'],
+        status: json['status']);
+  }
+
+  static List<Donation> fromJsonArray(String jsonData) {
+    final Iterable<dynamic> data = jsonDecode(jsonData);
+    return data.map<Donation>((dynamic d) => Donation.fromJson(d)).toList();
+  }
+
+  Map<String, dynamic> toJson(Donation donation) {
+    return {
+      'id': donation.id,
+      'donor': donation.donor,
+      'category': donation.category,
+      'weight': donation.weight,
+      'address': donation.address,
+      'contactNo': donation.contactNo,
+      'pickUpDateTime': donation.pickUpDateTime,
+      'dropOffDateTime': donation.dropOffDateTime,
+      'photo': donation.photo,
+      'status': donation.status
+    };
+  }
 }
