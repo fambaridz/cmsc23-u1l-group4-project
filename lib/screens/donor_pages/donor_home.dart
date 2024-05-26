a4ff6b6a66ec002a81274f5ec0cac823699f83d4
Famela Barida <fdbarida@up.edu.ph>
2024-05-26 21:33:35 +0800

    feat: added api to always get the latest user data from the database

--- lib/screens/donor_pages/donor_home.dart
+++ lib/screens/donor_pages/donor_home.dart
@@ -1,15 +1,20 @@
 import 'package:cmsc23_project/screens/donor_pages/donor_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cmsc23_project/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class DonorHomePage extends StatefulWidget {
-  final Map<String, dynamic> userData;
-  const DonorHomePage({super.key, required this.userData});
+  const DonorHomePage({super.key});
 
   @override
   State<DonorHomePage> createState() => _DonorHomePageState();
 }
 
 class _DonorHomePageState extends State<DonorHomePage> {
+  late Map<String, dynamic> userData;
+  User? user;
+
   final List<String> organizations = [
     'Organization A',
     'Organization B',
@@ -17,63 +22,74 @@ class _DonorHomePageState extends State<DonorHomePage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
+    user = context.read<UserAuthProvider>().user;
+    if (user != null) {
+      context.read<UserAuthProvider>().getUserData(user!.uid).then((value) {
+      setState(() {
+        userData = value!;
+      });
+    });};
+    
     return Scaffold(
-      drawer: DonorDrawer(userData: widget.userData),
+      drawer: DonorDrawer(userData: userData),
       appBar: AppBar(
         title: Text("Donor Home"),
       ),
       body: Center(
         child: organizations.isEmpty
-            ? const Text("No organizations available")
-            : ListView(
-                padding: const EdgeInsets.all(10),
-                children: organizations
-                    .map((organization) => GestureDetector(
-                          onTap: () {
-                            print(widget.userData);
-                            Navigator.pushNamed(context, "/donor-org-details");
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.grey[100],
-                            ),
-                            margin: EdgeInsets.all(10),
-                            child: ListTile(
-                              contentPadding: EdgeInsets.all(20),
-                              title: Text(
-                                organization,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              trailing: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(
-                                      context, "/donor-donation",
-                                      arguments: widget.userData);
-                                },
-                                child: Text(
-                                  "Donate",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.lightBlue[200],
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              ),
-                            ),
+          ? const Text("No organizations available")
+          : ListView(
+              padding: const EdgeInsets.all(10),
+              children: organizations
+                .map((organization) => GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/donor-org-details");
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[100],
+                    ),
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.all(20),
+                      title: Text(
+                        organization,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/donor-donation", arguments: userData);
+                        },
+                        child: Text(
+                          "Donate",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ))
-                    .toList(),
-              ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlue[200],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ))
+                .toList(),
+            ),
       ),
     );
   }
